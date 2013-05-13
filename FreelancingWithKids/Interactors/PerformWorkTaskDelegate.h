069f8a1a5ac68c34a95e2b1926a65029e6e406dc
paytonrules <esmith@8thlight.com>
2013-05-13 07:21:44 -0500

    First test passing

---
+++ FreelancingWithKids/Interactors/PerformWorkTaskDelegate.h
@@ -0,0 +1,13 @@
+//
+//  PerformWorkTaskDelegate.h
+//  FreelancingWithKids
+//
+//  Created by Eric Smith on 5/13/13.
+//  Copyright (c) 2013 Eric Smith. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol PerformWorkTaskDelegate <NSObject>
+
+@end
